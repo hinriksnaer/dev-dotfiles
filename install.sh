031bdd510728965668e3335c66584a20b928db5e
Hinrik S. Gudmundsson <hgudmund@redhat.com>
2025-08-06 14:25:18 -0400

    added install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+######
+# install zsh plugins
+######
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
