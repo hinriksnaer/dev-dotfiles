b86506b5ec66206e2bfe54f770871964123e4350
hinriksnaer <hgudmund@redhat.com>
2025-09-19 18:19:52 +0000

    fixed tmux install

--- scripts/install-tmux.sh
+++ scripts/install-tmux.sh
@@ -6,11 +6,24 @@ DIR=$(if [ "$(id -u)" -eq 0 ]; then echo "/root"; else echo "/home/$(whoami)"; f
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-echo "→ Installing packages: btop"
+echo "→ Installing packages tmux"
 
 # Packages to install
 packages=(
-  btop
+  tmux
 )
 
-stow -t "$DIR" btop
+stow -t "$DIR" tmux
+
+echo "setting up tmux"
+# Setup tmux plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+token=tpm_done
+tmux start-server \; \
+  set -g exit-empty off \; \
+  source-file ~/.config/tmux/tmux.conf \; \
+  run-shell "~/.tmux/plugins/tpm/scripts/install_plugins.sh && tmux wait-for -S $token" \; \
+  wait-for "$token" \; \
+  set -g exit-empty on
+echo "tmux setup complete."
