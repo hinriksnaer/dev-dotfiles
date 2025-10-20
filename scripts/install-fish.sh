82aebd56730240182ebb86749cc086d5a7e3da81
hinriksnaer <hgudmund@redhat.com>
2025-10-20 14:29:38 +0000

    small fix

--- scripts/install-fish.sh
+++ scripts/install-fish.sh
@@ -7,11 +7,11 @@ DIR=$(if [ "$(id -u)" -eq 0 ]; then echo "/root"; else echo "/home/$(whoami)"; f
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 echo "→ Installing Fish and dependencies"
-#
-# sudo dnf copr enable -y atim/starship
-#
-# sudo dnf upgrade --refresh -qy
-# sudo dnf install -qy fish starship
+
+sudo dnf copr enable -y atim/starship
+
+sudo dnf upgrade --refresh -qy
+sudo dnf install -qy fish #starship
 #
 # # Try lsd via dnf first; fall back to cargo only if needed
 # if ! command -v lsd >/dev/null 2>&1; then
