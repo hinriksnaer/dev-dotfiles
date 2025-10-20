85529ace7ecf1d74d8fc54529bac73ee71757f2d
hinriksnaer <hgudmund@redhat.com>
2025-10-20 14:44:37 +0000

    fixed starship

--- scripts/install-fish.sh
+++ scripts/install-fish.sh
@@ -11,7 +11,7 @@ echo "→ Installing Fish and dependencies"
 sudo dnf copr enable -y atim/starship
 
 sudo dnf upgrade --refresh -qy
-sudo dnf install -qy fish #starship
+sudo dnf install -qy fish starship
 
 # # Try lsd via dnf first; fall back to cargo only if needed
 if ! command -v lsd >/dev/null 2>&1; then
